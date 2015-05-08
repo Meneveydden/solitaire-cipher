cb3b96ab04eb18d0d819855590c05b3722cc1dab
chilldude <tonychenkt@gmail.com>
2015-05-08 00:28:28 -0700

    random deck generator

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-import solitaire
-import helpers
-import card_strings
+from deck import *
+from helpers import *
+from card_strings import *
+from prompt_strings import *
 
+import sys
+
+def main():
+
+	# random deck generator
+	if sys.argv[1] == "generate":
+		if len(sys.argv) == 2:
+			d = Deck("full")
+			d.randomize()
+		else:
+			if sys.argv[2] == "half":
+				d = Deck("half")
+				d.randomize()
+			elif sys.argv[2] == "full":
+				d = Deck("full")
+				d.randomize()
+
+	d.print_deck()
+	#if sys.argv > 1:
+	#	print "yes"
+
+	# mode: 0 for decode, 1 for encode
+	mode = 1
+
+	print key_input_prompt
+
+if __name__ == "__main__":
+	main()
---
+++ prompt_strings.py
@@ -0,0 +1,17 @@
+key_input_prompt = """
+Please enter the order of your deck in comma separated values.
+
+Cards are formatted as <rank><suit>. For Jokers, use 'A' and 'B' (not case-sensitive)
+For example: AH, 3C, KD, B, 10S, and so on.
+
+
+
+By convention, the first element of the list is the top card of the deck and the last element of the list is the bottom card of the deck.
+
+"""
+
+
+
+mode_select_prompt = """
+
+"""
